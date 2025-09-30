6da091fdd9c1bed759ce3182a64863a838f8e24c
chanwoongp <chanwoong@burritowallet.com>
2025-09-30 15:54:05 +0900

    add utils

---
+++ utils/src/json/mod.rs
@@ -0,0 +1,8 @@
+pub fn build_rpc_body(method: &str, params: serde_json::Value) -> serde_json::Value {
+    serde_json::json!({
+        "jsonrpc": "2.0",
+        "method": method,
+        "params": params,
+        "id": 0
+    })
+}
