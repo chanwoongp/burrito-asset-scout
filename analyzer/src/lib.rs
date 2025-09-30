be4404b5e410cc2dabf941d4284673322f393248
chanwoongp <chanwoong@burritowallet.com>
2025-09-30 12:07:36 +0900

    add get

--- analyzer/src/lib.rs
+++ analyzer/src/lib.rs
@@ -1,6 +1,27 @@
 pub mod native_coin_transfer;
 pub mod token_transfer;
 
+use native_coin_transfer::ethereum::EthereumNativeCoinTransfer;
+use native_coin_transfer::solana::SolanaNativeCoinTransfer;
+use token_transfer::ethereum::EthereumTokenTransfer;
+use token_transfer::solana::SolanaTokenTransfer;
+
 pub trait Analyzer {
     fn analyze(&self);
+}
+
+pub fn get_analyzer(analyzer_type: &str, chain: &str) -> Option<Box<dyn Analyzer>> {
+    match analyzer_type {
+        "native_coin_transfer" => match chain {
+            "ethereum" => Some(Box::new(EthereumNativeCoinTransfer {})),
+            "solana" => Some(Box::new(SolanaNativeCoinTransfer {})),
+            _ => None,
+        },
+        "token_transfer" => match chain {
+            "ethereum" => Some(Box::new(EthereumTokenTransfer {})),
+            "solana" => Some(Box::new(SolanaTokenTransfer {})),
+            _ => None,
+        },
+        _ => None,
+    }
 }
