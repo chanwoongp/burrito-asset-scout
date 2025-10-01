1c03cbe973444aa05951a9eda30b433b4ea9e885
chanwoongp <chanwoong@burritowallet.com>
2025-10-01 16:00:16 +0900

    add solana model

--- provider/src/solana/model.rs
+++ provider/src/solana/model.rs
@@ -1,9 +1,17 @@
-use serde::Deserialize;
+use serde::{Deserialize, Serialize};
 
 #[derive(Debug, Deserialize)]
 pub struct Context {
     pub slot: u64,
 }
+
+#[derive(Serialize, Deserialize)]
+struct Status {
+    #[serde(rename = "Ok")]
+    pub ok: Option<bool>,
+}
+
+// GetLatestBlockHash
 
 #[derive(Debug, Deserialize)]
 pub struct GetLatestBlockHashResponse {
@@ -14,4 +22,57 @@ pub struct GetLatestBlockHashResponse {
 #[derive(Debug, Deserialize)]
 pub struct GetLatestBlockHashValue {
     pub blockhash: String,
+}
+
+// GetBlockResponse
+
+#[derive(Serialize, Deserialize)]
+struct GetBlockResponseTransactionAccountKey {
+    pub pubkey: String,
+    pub signer: bool,
+    pub source: String,
+    pub writable: bool,
+}
+
+#[derive(Serialize, Deserialize)]
+struct GetBlockResponseTransactionItem {
+    #[serde(rename = "accountKeys")]
+    pub account_keys: Vec<GetBlockResponseTransactionAccountKey>,
+    pub signatures: Vec<String>,
+}
+
+#[derive(Serialize, Deserialize)]
+struct GetBlockResponseTransactionMeta {
+    pub err: Option<String>,
+    pub fee: i64,
+    #[serde(rename = "postBalances")]
+    pub post_balances: Vec<i64>,
+    #[serde(rename = "postTokenBalances")]
+    pub post_token_balances: Vec<u64>,
+    #[serde(rename = "preBalances")]
+    pub pre_balances: Vec<i64>,
+    #[serde(rename = "preTokenBalances")]
+    pub pre_token_balances: Vec<u64>,
+    pub status: Status,
+}
+
+#[derive(Serialize, Deserialize)]
+struct GetBlockResponseTransaction {
+    pub meta: GetBlockResponseTransactionMeta,
+    pub transaction: GetBlockResponseTransactionItem,
+    pub version: String,
+}
+
+#[derive(Serialize, Deserialize)]
+struct GetBlockResponse {
+    #[serde(rename = "blockHeight")]
+    pub block_height: i64,
+    #[serde(rename = "blockTime")]
+    pub block_time: i64,
+    pub blockhash: String,
+    #[serde(rename = "parentSlot")]
+    pub parent_slot: i64,
+    #[serde(rename = "previousBlockhash")]
+    pub previous_blockhash: String,
+    pub transactions: Vec<GetBlockResponseTransaction>,
 }
